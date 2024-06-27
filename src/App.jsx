c5af060c7b9cb38442552e5e7d22ab216858065b
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-06-27 20:05:16 -0300

    chore: initit commit setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-slate-500 text-3xl font-bold underline ">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
