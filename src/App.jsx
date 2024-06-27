9e1065c965972aed0b071f5f5ba9d35fe64c7839
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-06-27 20:42:01 -0300

    chore: setup and configuration shadcnui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
+
+
 
 function App() {
   return (
     <>
       <div>
         <h1 className="bg-slate-500 text-3xl font-bold underline ">Hello World</h1>
+      </div>
+      <div>
+        <Button>Clique</Button>
       </div>
     </>
   );
