9e2b83905623542f961a2d81fc117b5fd55cbf77
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-07-02 14:48:50 -0300

    feat: buttons added, part of the header finalized, added font family

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
-
 
 
 function App() {
   return (
     <>
       <div>
-        <h1 className="bg-slate-500 text-3xl font-bold underline ">Hello World</h1>
-      </div>
-      <div>
-        <Button>Clique</Button>
+        <h1>hello</h1>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,15 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: "0px",
       screens: {
         "2xl": "1400px",
       },
     },
     extend: {
+      fontFamily: {
+        
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
