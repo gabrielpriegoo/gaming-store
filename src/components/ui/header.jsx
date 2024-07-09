50f6f8fd282c2edec205f394db20a9f956d82e86
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-07-09 10:31:35 -0300

    feat: add event button nav menu

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -1,13 +1,24 @@
 import { HomeIcon, ListOrderedIcon, MenuIcon, PercentIcon, ShoppingCartIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 import { Link } from "react-router-dom";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [open, setOpen] = useState(false)
+
+  const navigate = useNavigate()
+  
+  const handleNavigation = (path) => {
+    setOpen(false)
+    navigate(path)
+  }
+
   return (
     <Card className="flex justify-between p-[1.875rem]">
-     <Sheet>
+     <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
       <Button size="icon" variant="outline">
         <MenuIcon />
@@ -17,9 +28,9 @@ const Header = () => {
       <SheetContent side="left">
         <SheetHeader className="text-left text-lg font-semibold">Menu</SheetHeader>
         <div className="mt-2 flex flex-col gap-2">
-          <Button variant="outline" className="w-full justify-start gap-2"><HomeIcon size={16} />Home</Button>
-          <Button variant="outline" className="w-full justify-start gap-2"><PercentIcon size={16} />Consoles</Button>
-          <Button variant="outline" className="w-full justify-start gap-2"><ListOrderedIcon size={16} />Jogos</Button>
+          <Button variant="outline" className="w-full justify-start gap-2" onClick={() => handleNavigation("/") }><HomeIcon size={16} />Home</Button>
+          <Button variant="outline" className="w-full justify-start gap-2" onClick={() => handleNavigation("/consoles") }><PercentIcon size={16} />Consoles</Button>
+          <Button variant="outline" className="w-full justify-start gap-2" onClick={() => handleNavigation("/jogos") }><ListOrderedIcon size={16} />Jogos</Button>
         </div>
       </SheetContent>
      </Sheet>
